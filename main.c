7386ff0b2844f5fcea7a5659ec8772fe263f3550
Bk-San1201 <45562321+Bk-San1201@users.noreply.github.com>
2018-12-19 22:09:01 +0700

    BTVN

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+struct infoStudent {
+	char firstName[100];
+	char lastName[50];
+	long long int id;
+	int birthDay; int birthMonth; int birthYear;
+	float midMarks, finalMarks;
+}
